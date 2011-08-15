0d4a55fac661babf19ddd056e10a9a76d039e606
Lee Wei <evandrix@gmail.com>
2011-08-15 19:31:24 +0100

    updated banner image & translate titlebar text to indonesian & update promo graphics

--- gen/net/terang/dunia/gospels/in/unison/R.java
+++ gen/net/terang/dunia/gospels/in/unison/R.java
@@ -29,5 +29,6 @@ public final class R {
         public static final int app_name=0x7f040000;
         public static final int database_name=0x7f040001;
         public static final int open_helper_classname=0x7f040002;
+        public static final int titlebar_text=0x7f040003;
     }
 }
